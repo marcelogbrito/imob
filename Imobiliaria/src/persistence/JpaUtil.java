31d05589e01cae7da6b9af64a57edf1acf5ab876
Marcelo Brito <Marcelo Brito@MarceloBrito-PC>
2013-11-08 00:04:09 -0200

    commit 3

--- Imobiliaria/src/persistence/JpaUtil.java
+++ Imobiliaria/src/persistence/JpaUtil.java
@@ -7,46 +7,15 @@ import javax.persistence.Persistence;
 
 public class JpaUtil {
 	
-	private static final String PERSISTENCE_UNIT_NAME = "default";
-	
-	private static EntityManagerFactory factory;
-	
-	private static ThreadLocal<EntityManager> manager = new ThreadLocal<EntityManager>();
-	
-	public static boolean isEntityManagerOpen(){
-		return JpaUtil.manager.get() != null && JpaUtil.manager.get().isOpen();
+	private static EntityManagerFactory emf = Persistence
+			.createEntityManagerFactory("default");
+
+	public EntityManager getEntityManager() {
+		return emf.createEntityManager();
 	}
 
-	
-	public static EntityManager getEntityManager(){
-		if(JpaUtil.factory == null){
-			JpaUtil.factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-		}
-		EntityManager em = JpaUtil.manager.get();
-		if(em == null || !em.isOpen()){
-			em = JpaUtil.factory.createEntityManager();
-			JpaUtil.manager.set(em);
-		}
-		return em;
+	public void close(EntityManager em) {
+		em.close();
 	}
-	
-	public static void closeEntityManager(){
-		EntityManager em = JpaUtil.manager.get();
-		if(em != null){
-			EntityTransaction tx = em.getTransaction();
-			if(tx.isActive()){
-				tx.commit();
-			}
-			em.close();
-			JpaUtil.manager.set(null);
-		}
-	}
-	
-	public static void closeEntityManagerFactory(){
-		closeEntityManager();
-		JpaUtil.factory.close();
-	}
-	
-	
 
 }
